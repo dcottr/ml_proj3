21f53289588b853ec5a87bc4f1c5f371aa79bc3a
David Cottrell <dcottrell@macbook-pro.gateway.2wire.net>
2015-10-27 21:56:37 -0400

    logistic first commit

---
+++ scripts/logistic_learner.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+from sklearn import linear_model
+from sklearn.pipeline import Pipeline
+from sklearn.grid_search import GridSearchCV
+
+logistic = linear_model.LogisticRegression()
